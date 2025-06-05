7613d7d695c5abdebd3e5693dd2129bb1be8b64a
kaytee07 <kofi.taylor07@gmail.com>
2025-06-05 00:35:47 +0000

    refactor: add method declarations for findByProjectId, findByDeveloperId in TaskRepository

--- src/main/java/taylor/project/projecttracker/Repository/TaskRepository.java
+++ src/main/java/taylor/project/projecttracker/Repository/TaskRepository.java
@@ -3,5 +3,9 @@ package taylor.project.projecttracker.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import taylor.project.projecttracker.Entity.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProjectId(Long projectId);
+    List<Task> findByDeveloperId(Long developerId);
 }
