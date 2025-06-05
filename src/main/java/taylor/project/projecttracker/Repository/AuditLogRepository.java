2ee8ddd9050e413c93cd76b4c0db8c74f99e0765
kaytee07 <kofi.taylor07@gmail.com>
2025-06-05 00:34:03 +0000

    refactor: add method declarations for findByEntityType, findByActorName in AuditLogRepository

--- src/main/java/taylor/project/projecttracker/Repository/AuditLogRepository.java
+++ src/main/java/taylor/project/projecttracker/Repository/AuditLogRepository.java
@@ -3,5 +3,10 @@ package taylor.project.projecttracker.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import taylor.project.projecttracker.Entity.AuditLog;
 
+import java.util.List;
+
 public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
+    List<AuditLog> findByEntityType(String entityType);
+    List<AuditLog> findByActorName(String actorName);
 }
+
